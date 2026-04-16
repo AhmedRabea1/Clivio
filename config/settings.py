66615e95d9a965a24394a5eb0808518b5e952ffa
Ahmed Rabea <ahmedrabea@Ahmeds-MacBook-Air.local>
2026-04-16 20:36:58 +0200

    add api test page and fix cors for port 5500

--- config/settings.py
+++ config/settings.py
@@ -159,10 +159,12 @@ CORS_ALLOWED_ORIGINS += [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:4200',
+    'http://localhost:5500',
     'http://localhost:8080',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:4200',
+    'http://127.0.0.1:5500',
     'http://127.0.0.1:8080',
 ]
 CORS_ALLOWED_ORIGINS = list(set(CORS_ALLOWED_ORIGINS))  # remove duplicates
