febeeeb7521f66035477e61be03ded7206804795
Ahmed Rabea <ahmedrabea@Ahmeds-MacBook-Air.local>
2026-04-16 20:43:40 +0200

    allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-producti
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -159,12 +159,10 @@ CORS_ALLOWED_ORIGINS += [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:4200',
-    'http://localhost:5500',
     'http://localhost:8080',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:4200',
-    'http://127.0.0.1:5500',
     'http://127.0.0.1:8080',
 ]
 CORS_ALLOWED_ORIGINS = list(set(CORS_ALLOWED_ORIGINS))  # remove duplicates
