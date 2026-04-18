cd1ac09bf00cc0668d3e1a54c4be0bd151cbf676
Ahmed Rabea <ahmedrabea@Ahmeds-MacBook-Air.local>
2026-04-18 19:48:45 +0200

    fix urls images

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.shortcuts import redirect
 
 # API imports
@@ -48,4 +49,6 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('branches.urls')),
     path('', include('appointments.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
