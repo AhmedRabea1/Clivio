61fed7088dbb49f4d3361c26728ffa4537d40446
Ahmed Rabea <ahmedrabea@Ahmeds-MacBook-Air.local>
2026-05-03 00:28:21 +0300

    icrease workers

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
